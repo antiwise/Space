3150a4e4b2c663214f2d932e4fe723ede52c1b9d
Gonzalo Diaz Cruz <gonzalo@studiopangea.com>
2012-09-25 18:37:55 -0300

    Project template for Cocos2D-x 2.0 hybrid project (Android/iOS)

---
+++ proj.android/gen/com/studiopangea/demo/ProjectTemplate/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.studiopangea.demo.ProjectTemplate;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
